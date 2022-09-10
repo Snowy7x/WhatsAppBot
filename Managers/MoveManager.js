6276d86318a62c63ec0da741dfd33c16332994a6
Snowy7 <snowydev7@gmail.com>
2022-09-10 10:43:21 +0300

    Adding movies and 3mk

--- Managers/MoveManager.js
+++ Managers/MoveManager.js
@@ -7,7 +7,14 @@ const getMoveDetails = async (movieName) => {
         data = da;
         console.log("got the data")
     })
-    let desc = await translate(data.overview, {from: "en", to: "ar"})
+    let desc;
+    await translate(data.overview, {from: "en", to: "ar"}).then((da) => {
+        console.log("got the desc")
+        desc = da
+    }).catch((err) => {
+        console.log("something wrong:", err)
+        desc = data.overview
+    })
     console.log("got the desc")
     return {
         name: data.title,
