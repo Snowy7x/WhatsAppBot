745ed15c31fdfde3b9f76e7b26ed9c061e34901e
Snowy7 <snowydev7@gmail.com>
2022-11-13 14:06:51 +0300

    Fixed bugs adding new groups

--- src/index.js
+++ src/index.js
@@ -20,7 +20,6 @@ const client = new Client({
             '--disable-dev-shm-usage',
             '--disable-accelerated-2d-canvas',
             '--no-first-run',
-            '--no-zygote',
             '--single-process',
             '--disable-gpu',
         ],
