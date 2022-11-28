04067c16a990a610f42a032fbced9914e9b504e6
Snowy7 <snowydev7@gmail.com>
2022-11-28 17:46:26 +0300

    Fixed bugs adding new groups

--- src/index.js
+++ src/index.js
@@ -19,12 +19,6 @@ const client = new Client({
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-first-run',
-            '--no-zygote',
-            '--single-process',
-            '--disable-gpu',
         ],
     },
     ffmpegPath: "/usr/bin/ffmpeg",
